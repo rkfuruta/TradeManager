ad3e34497237723ea8768a2768444af42ae7100c
RKFuruta <rcontas@gmail.com>
2023-02-24 22:48:44 -0300

    :sparkles: Created transaction routes

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 const User = require("./src/controllers/user.js");
 const Inventory = require("./src/controllers/inventory.js");
 const Item = require("./src/controllers/item.js");
+const Dashboard = require("./src/controllers/dashboard.js");
 const auth = require("./src/middleware/auth.js");
 
 // USERS ENDPOINT
@@ -17,5 +18,9 @@ routes.get("/v1/inventory",  auth, Inventory.get);
 
 // ITEM ENDPOINT
 routes.post("/v1/item/:id/status",  auth, Item.status);
+
+//DASHBOARD ENDPOINT
+routes.get("/v1/dashboard/transactions",  auth, Dashboard.transactions);
+routes.get("/v1/dashboard/transactions/dates",  auth, Dashboard.transactionDates);
 
 module.exports = routes;
---
+++ src/controllers/dashboard.js
@@ -0,0 +1,37 @@
+const moment = require("moment");
+const Inventory = require("../models/inventory.js");
+const InventoryItem = require("../models/inventory_item.js");
+const { Op } = require("sequelize");
+const db = require("../db.js");
+
+async function transactions(req, res) {
+    if (!req.query.month || !req.query.year) {
+        return res.status(400).json({ success: false, message: "Params 'month' and 'year' are required"});
+    }
+    const tokenData = req.token_data;
+    const userId = tokenData.user_id;
+    let start = moment.utc(`${req.query.month}/${req.query.year}`,"MM/YYYY").startOf('month');
+    let end = moment.utc(`${req.query.month}/${req.query.year}`,"MM/YYYY").endOf('month');
+    let inventory = await Inventory.findOne({ where: { user_id: userId }});
+    if (!inventory) {
+        return res.status(200).json({ success: false, message: "No data available"});
+    }
+    let items = await InventoryItem.findAll({
+        where: {
+            inventory_id: inventory.entity_id,
+            createdAt: { [Op.between]: [start.toISOString(), end.toISOString()] },
+            purchase_date: { [Op.ne]: null }
+        },
+        order: [ ["purchase_date", "DESC"] ]
+    });
+    return res.status(200).json({ success: true, data: {items}});
+}
+async function transactionDates(req, res) {
+    const [results, metadata] = await db.query("SELECT DISTINCT YEAR(purchase_date) AS Year, MONTH(purchase_date) AS Month FROM inventory_items WHERE purchase_date IS NOT NULL ORDER BY Year, Month DESC");
+    return res.status(200).json({ success: true, data: {results}});
+}
+
+module.exports = {
+    transactions,
+    transactionDates
+}
