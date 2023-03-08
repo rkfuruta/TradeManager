fd0e80bc290531cc66da924d9558a4093b84e07c
RKFuruta <rcontas@gmail.com>
2023-03-08 20:16:28 -0300

    :sparkles: Added order and filter to logs list endpoint

--- src/controllers/bot.js
+++ src/controllers/bot.js
@@ -1,19 +1,25 @@
 const Log = require("../models/log");
+const { Op } = require("sequelize");
 
 async function all(req, res) {
     const params = req.query;
     let page = 1;
-    let limit = 100;
+    let limit = 10;
     if (params.hasOwnProperty("page")) {
         page = parseInt(params.page);
     }
     if (params.hasOwnProperty("limit")) {
         limit = parseInt(params.limit);
     }
-    const { count, rows } = await Log.findAndCountAll({
+    let condition = {
+        order: [["entity_id", "DESC"]],
         offset: --page*limit,
         limit: limit
-    });
+    };
+    if (params.hasOwnProperty("code")) {
+        condition.where = { code: {[Op.in]: params.code.split(",") }}
+    }
+    const { count, rows } = await Log.findAndCountAll(condition);
     return res.status(200).json(
         {
             success: true ,
