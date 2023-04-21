cb96e71d09c081d519e4a85b03b91ad279e4eee4
RKFuruta <rcontas@gmail.com>
2023-04-21 12:43:10 -0300

    :bug: Added trycatch to price endpoint

--- src/controllers/price.js
+++ src/controllers/price.js
@@ -3,11 +3,15 @@ const fs = require("fs");
 const fileLocation = __dirname + "/../../priceempire.json";
 
 async function get(req, res) {
-    const file = fs.readFileSync(fileLocation);
-    if (file) {
-        res.status(200).json(JSON.parse(file));
-    } else {
-        res.status(401).json({ success: false, message: "Invalid Credentials"});
+    try {
+        const file = fs.readFileSync(fileLocation);
+        if (file) {
+            res.status(200).json(JSON.parse(file));
+        } else {
+            res.status(401).json({ success: false, message: "Invalid Credentials"});
+        }
+    } catch (e) {
+        res.status(500).json({ success: false, message: e.message });
     }
 }
 
