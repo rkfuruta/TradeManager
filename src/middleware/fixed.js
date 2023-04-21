8fb9da34c509c140f6bda1f4c2f0d56422b59744
RKFuruta <rcontas@gmail.com>
2023-04-21 13:20:07 -0300

    :sparkles: Allowing token throygh query param

--- src/middleware/fixed.js
+++ src/middleware/fixed.js
@@ -1,7 +1,7 @@
 const FIXED_TOKEN = "Oe68BS2yGgbBA7RSaw5qthXT6zVWvaPG";
 
 const verifyToken = (req, res, next) => {
-    const authorization = req.headers["authorization"];
+    const authorization = req.headers["authorization"] || req.query["token"];
     if (!authorization) {
         return res.status(403).json({ success: false, message: "A token is required for authentication"});
     }
