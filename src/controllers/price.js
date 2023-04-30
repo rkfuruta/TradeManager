7ed10dd8d16352e97f8d56f9a43ff0acd40bd2bd
RKFuruta <rcontas@gmail.com>
2023-04-30 18:17:19 -0300

    :sparkles: Added error return on price API in case file gets 2 or more hours old

--- src/controllers/price.js
+++ src/controllers/price.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const moment = require("moment");
 
 const fileLocation = __dirname + "/../../priceempire.json";
 
@@ -6,7 +7,18 @@ async function get(req, res) {
     try {
         const file = fs.readFileSync(fileLocation);
         if (file) {
-            res.status(200).json(JSON.parse(file));
+            const json = JSON.parse(file);
+            if (json.hasOwnProperty("created_at")) {
+                const created_at = moment(json.created_at);
+                const valid = moment().subtract(2, 'hours');
+                if (created_at.isBefore(valid)) {
+                    res.status(503).json({ success: false, message: `Last price update occurred more than 2 hours ago. Last update ${created_at.format('YYYY-MM-DD HH:mm:ss')}` });
+                } else {
+                    res.status(200).json(json);
+                }
+            } else {
+                res.status(500).json({ success: false });
+            }
         } else {
             res.status(401).json({ success: false, message: "Invalid Credentials"});
         }
